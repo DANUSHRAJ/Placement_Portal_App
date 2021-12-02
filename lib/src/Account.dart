f6833f7fc4f5b96dd69de83d2dde2e9dbfc9a991
KaarTechAS <94947062+KaarTechAS@users.noreply.github.com>
2021-12-02 10:38:00 +0530

    Anish Changes. Batch Included in Signup form. Batch only in accounts coll.

--- lib/src/Account.dart
+++ lib/src/Account.dart
@@ -4,8 +4,9 @@ class Account {
   final String regno;
   final String username;
   final String password;
+  final String batch;
 
-  const Account._(this.id, this.name, this.regno, this.username, this.password);
+  const Account._(this.id, this.name, this.regno, this.username, this.password, this.batch);
 
   factory Account.fromJson(Map json) {
     final id = json['_id'].replaceAll('ObjectId(\"', '').replaceAll('\")', '');
@@ -13,6 +14,7 @@ class Account {
     final regno = json['regno'];
     final username = json['username'];
     final password = json['password'];
-    return Account._(id, name, regno, username, password);
+    final batch = json['batch'];
+    return Account._(id, name, regno, username, password, batch);
   }
 }
