6d0bc75d4b23f7d15eaa9800879b09df29ba2812
DANUSHRAJ <mldanushraj2001@gmail.com>
2021-12-31 11:45:08 +0530

    commit before pull-31-12-2021

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'package:SJIT_PLACEMENT_PORTAL/src/welcomePage.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the root of your application. 
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
