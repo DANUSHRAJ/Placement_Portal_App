4ff9341530ec38306b557fe8299a77f8b513ac00
JOHNJNK <johnjnk2000@gmail.com>
2022-01-07 11:59:30 +0530

    General data assigning values

--- lib/src/PP_CurrentEducation.dart
+++ lib/src/PP_CurrentEducation.dart
@@ -24,52 +24,17 @@ class NewObject {
 class _PpCurrentDState extends State<PpCurrentD> {
   bool loading = false;
 
-  static final List<NewObject> title = <NewObject>[
-    NewObject('SELECT TITLE', Icons.description),
-    NewObject('Mr', Icons.person),
-    NewObject('Ms', Icons.person),
-  ];
-  static final List<NewObject> gender = <NewObject>[
-    NewObject('SELECT GENDER', Icons.person),
-    NewObject('MALE', Icons.male_rounded),
-    NewObject('FEMALE', Icons.female_rounded),
+  List<String> pce=[
+    'null','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null','null'
   ];
 
-  static final List<NewObject> college = <NewObject>[
-    NewObject('SELECT COLLEGE', Icons.description),
-    NewObject('SJIT', Icons.home_outlined),
-    NewObject('SJCE', Icons.home_outlined),
-  ];
-
-  static final List<NewObject> department = <NewObject>[
-    NewObject('SELECT DEPARTMENT', Icons.description),
-    NewObject('B.Tech IT', Icons.mobile_friendly_rounded),
-    NewObject('B.E CSE', Icons.computer_rounded),
-    NewObject('B.E ECE', Icons.signal_cellular_alt_outlined),
-    NewObject('B.E EEE', Icons.lightbulb_outline_rounded),
-    NewObject('B.E MECH', Icons.settings_applications_rounded),
-    NewObject('B.E CIVIL', Icons.apartment_outlined),
-  ];
-
-  static final List<NewObject> section = <NewObject>[
-    NewObject('SELECT SECTION', Icons.description),
-    NewObject('A', Icons.arrow_back_ios),
-    NewObject('B', Icons.arrow_back_ios),
-    NewObject('C', Icons.arrow_back_ios),
-  ];
   static final List<NewObject> yesorno = <NewObject>[
     NewObject('SELECT THE OPTION', Icons.description),
     NewObject('YES', Icons.arrow_back_ios),
     NewObject('NO', Icons.arrow_back_ios),
   ];
-  static final List<NewObject> dropbox = <NewObject>[
-    title.first, //0
-    gender.first, //1
-    college.first, //2
-    department.first, //3
-    section.first, //4//11
-    yesorno.first,
-  ];
+
+  NewObject no_arrears=yesorno.first;
 
   Widget _backButton() {
     return InkWell(
@@ -91,125 +56,7 @@ class _PpCurrentDState extends State<PpCurrentD> {
     );
   }
 
-  Widget _entryFieldalphabetsdisplay(String title, String hint, int i,
-      {bool isPassword = false}) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10),
-      width: 500,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            title,
-            style: GoogleFonts.portLligatSans(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.limeAccent,
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-                hintText: hint,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                  borderSide: BorderSide(
-                    width: 0,
-                    style: BorderStyle.none,
-                  ),
-                ),
-                fillColor: Color(0xfff3f3f4),
-                filled: true),
-            enabled: false,
-            // onChanged: (value) => setState(() => pp[i] = value),
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget _entryFieldalphabets(String title, String hint, int i,
-      {bool isPassword = false}) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10),
-      width: 500,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            title,
-            style: GoogleFonts.portLligatSans(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.limeAccent,
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-                hintText: hint,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                  borderSide: BorderSide(
-                    width: 0,
-                    style: BorderStyle.none,
-                  ),
-                ),
-                fillColor: Color(0xfff3f3f4),
-                filled: true),
-            // onChanged: (value) => setState(() => pp[i] = value),
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget _entryFieldDob(String title, String hint, int i,
-      {bool isPassword = false}) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10),
-      width: 500,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            title,
-            style: GoogleFonts.portLligatSans(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.limeAccent,
-            ),
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-                prefixIcon: Icon(Icons.calendar_today),
-                hintText: hint,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                  borderSide: BorderSide(
-                    width: 0,
-                    style: BorderStyle.solid,
-                  ),
-                ),
-                fillColor: Color(0xfff3f3f4),
-                filled: true),
-            keyboardType: TextInputType.number,
-            // onChanged: (value) => setState(() => pp[i] = value),
-            // keyboardType: TextInputType.number,
-            // inputFormatters: <TextInputFormatter>[
-            //   FilteringTextInputFormatter.digitsOnly
-            // ],
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget _DropBox(String title, List<NewObject> key, int i) {
+  Widget _DropBox(String title, List<NewObject> key,) {
     return Container(
       margin: EdgeInsets.symmetric(vertical: 10),
       width: 500,
@@ -237,7 +84,7 @@ class _PpCurrentDState extends State<PpCurrentD> {
             ),
             child: DropdownButtonHideUnderline(
               child: DropdownButton<NewObject>(
-                value: dropbox.elementAt(i), // currently selected item
+                value: no_arrears, // currently selected item
                 items: key
                     .map((item) => DropdownMenuItem<NewObject>(
                   child: Row(
@@ -256,7 +103,7 @@ class _PpCurrentDState extends State<PpCurrentD> {
                   value: item,
                 ))
                     .toList(),
-                //onChanged: (value) => setState(() {dropbox[i] = value;}),
+                onChanged: (value) => setState(() {no_arrears = value;}),
               ),
             ),
           )
@@ -298,7 +145,7 @@ class _PpCurrentDState extends State<PpCurrentD> {
                 filled: true),
             keyboardType: TextInputType.number,
 
-            //onChanged: (value) => setState(() => pp[i] = value),
+            onChanged: (value) => setState(() => pce[i] = value),
 
             // inputFormatters: <TextInputFormatter>[
             //   FilteringTextInputFormatter.digitsOnly
@@ -373,39 +220,43 @@ class _PpCurrentDState extends State<PpCurrentD> {
                         SizedBox(height: height * .05),
                         Align(alignment: Alignment.center, child: _title()),
                         SizedBox(height: height * .1),
-                        _entryFieldnumbers('SEM1 GPA', 'Eg:7.12', 26),
-                        _entryFieldnumbers('SEM2 GPA', 'Eg:7.12', 27),
-                        _entryFieldnumbers('SEM3 GPA', 'Eg:7.12', 28),
-                        _entryFieldnumbers('SEM4 GPA', 'Eg:7.12', 29),
-                        _entryFieldnumbers('SEM5 GPA', 'Eg:7.12', 30),
-                        _entryFieldnumbers('SEM6 GPA', 'Eg:7.12', 31),
-                        _entryFieldnumbers('SEM7 GPA', 'Eg:7.12', 32),
-                        _entryFieldnumbers('SEM8 GPA', 'Eg:7.12', 33),
-                        _entryFieldnumbers('OVERALL GPA', 'Eg:7.12', 34),
+                        _entryFieldnumbers('SEM1 GPA', 'Eg:7.12', 0),
+                        _entryFieldnumbers('SEM2 GPA', 'Eg:7.12', 1),
+                        _entryFieldnumbers('SEM3 GPA', 'Eg:7.12', 2),
+                        _entryFieldnumbers('SEM4 GPA', 'Eg:7.12', 3),
+                        _entryFieldnumbers('SEM5 GPA', 'Eg:7.12', 4),
+                        _entryFieldnumbers('SEM6 GPA', 'Eg:7.12', 5),
+                        _entryFieldnumbers('SEM7 GPA', 'Eg:7.12', 6),
+                        _entryFieldnumbers('SEM8 GPA', 'Eg:7.12', 7),
+                        _entryFieldnumbers('OVERALL GPA', 'Eg:7.12', 8),
                         _entryFieldnumbers('NO OF ARREARS SEM 1',
-                            'if there is no arrears enter 0', 35),
+                            'if there is no arrears enter 0', 9),
                         _entryFieldnumbers('NO OF ARREARS SEM 2',
-                            'if there is no arrears enter 0', 36),
+                            'if there is no arrears enter 0', 10),
                         _entryFieldnumbers('NO OF ARREARS SEM 3',
-                            'if there is no arrears enter 0', 37),
+                            'if there is no arrears enter 0', 11),
                         _entryFieldnumbers('NO OF ARREARS SEM 4',
-                            'if there is no arrears enter 0', 38),
+                            'if there is no arrears enter 0', 12),
                         _entryFieldnumbers('NO OF ARREARS SEM 5',
-                            'if there is no arrears enter 0', 39),
+                            'if there is no arrears enter 0', 13),
                         _entryFieldnumbers('NO OF ARREARS SEM 6',
-                            'if there is no arrears enter 0', 40),
+                            'if there is no arrears enter 0', 14),
                         _entryFieldnumbers('NO OF ARREARS SEM 7',
-                            'if there is no arrears enter 0', 41),
+                            'if there is no arrears enter 0', 15),
                         _entryFieldnumbers('NO OF ARREARS SEM 8',
-                            'if there is no arrears enter 0', 42),
+                            'if there is no arrears enter 0', 16),
                         _entryFieldnumbers('TOTAL NO OF STANDING ARREARS',
-                            'if there is no arrears enter 0', 43),
-                        _DropBox("HISTORY OF ARREARS [Y/N]", yesorno, 5),
+                            'if there is no arrears enter 0', 17),
+                        _DropBox("HISTORY OF ARREARS [Y/N]", yesorno),
                         _entryFieldnumbers('IF YES, HOW MANY?',
-                            'if there is no arrears enter 0', 44),
+                            'if there is no arrears enter 0', 18),
                         Align(
                           alignment: Alignment.bottomRight,
                           child: FloatingActionButton(onPressed: () {
+                            print("------");
+                            for(int i=0;i<pce.length;i++){
+                              print(pce[i]);
+                            }
                             Navigator.push(
                                 context,
                                 PageTransition(
