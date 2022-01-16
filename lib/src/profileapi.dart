9939c10ced0e62ece94159c86bcf2a48f506f010
DANUSHRAJ <mldanushraj2001@gmail.com>
2022-01-16 21:08:57 +0530

    heroku profile final push

--- lib/src/profileapi.dart
+++ lib/src/profileapi.dart
@@ -16,7 +16,7 @@ import 'ExtraDJSON.dart';
 
 class ProfileApi {
 //  static String apiUrl = 'https://6081-27-57-63-29.ngrok.io';http://127.0.0.1:8081
-  static String apiUrl = 'http://127.0.0.1:8081';
+  static String apiUrl = 'https://sppdemo.herokuapp.com';
 
   final _dio = Dio(BaseOptions(baseUrl: apiUrl));
 
