c7e6a89c0e41b18c1c5b86015f4b833bf509a90a
DANUSHRAJ <mldanushraj2001@gmail.com>
2022-01-19 12:21:07 +0530

    commented commit

--- lib/src/wsapi.dart
+++ lib/src/wsapi.dart
@@ -1,43 +1,43 @@
-import 'dart:developer';
+// import 'dart:developer';
 
-import 'package:dio/dio.dart';
+// import 'package:dio/dio.dart';
 
-import 'IWCDetails.dart';
+// import 'IWCDetails.dart';
 
-class WorkshopApi {
-//  static String apiUrl = 'https://6081-27-57-63-29.ngrok.io';
-  static String apiUrl = 'http://127.0.0.1:8081';
-  final _dio = Dio(BaseOptions(baseUrl: apiUrl));
+// class WorkshopApi {
+// //  static String apiUrl = 'https://6081-27-57-63-29.ngrok.io';
+//   static String apiUrl = 'http://127.0.0.1:8081';
+//   final _dio = Dio(BaseOptions(baseUrl: apiUrl));
 
-  Future<List<IWCDetails>> getAccounts() async {
-    final response = await _dio.get('/getworkshop');
-    return (response.data['workshop'] as List)
-        .map<IWCDetails>((json) => IWCDetails.fromJson(json))
-        .toList();
-  }
+//   Future<List<IWCDetails>> getAccounts() async {
+//     final response = await _dio.get('/getworkshop');
+//     return (response.data['workshop'] as List)
+//         .map<IWCDetails>((json) => IWCDetails.fromJson(json))
+//         .toList();
+//   }
 
-  Future<IWCDetails> uploadWorkshop(
-      String regno,
-      String username,
-      String title,
-      String name,
-      String sd,
-      String ed,
-      String clink,
-      String plink,
-      String flink) async {
-    final response = await _dio.post('/uploadworkshop', data: {
-      'regno': regno,
-      'username': username,
-      'title': title,
-      'name': name,
-      'sd': sd,
-      'ed': ed,
-      'clink': clink,
-      'plink': plink,
-      'flink': flink
-    });
-//    log('$name-$regno-$username-$password');
-    return IWCDetails.fromJson(response.data);
-  }
-}
+//   Future<IWCDetails> uploadWorkshop(
+//       String regno,
+//       String username,
+//       String title,
+//       String name,
+//       String sd,
+//       String ed,
+//       String clink,
+//       String plink,
+//       String flink) async {
+//     final response = await _dio.post('/uploadworkshop', data: {
+//       'regno': regno,
+//       'username': username,
+//       'title': title,
+//       'name': name,
+//       'sd': sd,
+//       'ed': ed,
+//       'clink': clink,
+//       'plink': plink,
+//       'flink': flink
+//     });
+// //    log('$name-$regno-$username-$password');
+//     return IWCDetails.fromJson(response.data);
+//   }
+// }
