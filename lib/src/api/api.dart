f22d77be1d6956e4090cfd83373f1e9786b31b17
DANUSHRAJ <mldanushraj2001@gmail.com>
2022-01-23 20:31:42 +0530

    SERVER HEROKU COMMIT

--- lib/src/api/api.dart
+++ lib/src/api/api.dart
@@ -8,7 +8,7 @@ import '../JSON/Account.dart';
 class AccountsApi {
 //  static String apiUrl = 'https://6081-27-57-63-29.ngrok.io'  'http://127.0.0.1:8081'  'https://sppdemo.herokuapp.com';
 
-  static String apiUrl = 'https://sppdemo.herokuapp.com';
+  static String apiUrl = 'https://sjit-placement-portal.herokuapp.com';
 
   final _dio = Dio(BaseOptions(baseUrl: apiUrl));
 
--- lib/src/api/internapi.dart
+++ lib/src/api/internapi.dart
@@ -7,7 +7,7 @@ import '../JSON/IWCDetails.dart';
 class InternsApi {
 //  static String apiUrl = 'https://6081-27-57-63-29.ngrok.io'  'http://127.0.0.1:8081';
 
-  static String apiUrl = 'https://sppdemo.herokuapp.com';
+  static String apiUrl = 'https://sjit-placement-portal.herokuapp.com';
 
   final _dio = Dio(BaseOptions(baseUrl: apiUrl));
 
